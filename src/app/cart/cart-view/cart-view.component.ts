183393fed902fc9607975300e8da6955dc876064
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-04-26 23:46:31 +0530

    cart view component ts file updated.

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -10,6 +10,7 @@ import { Product } from 'src/app/models/product';
 export class CartViewComponent implements OnInit {
   
   cartItems : Product[] = [];
+  totalPrice : number = 0;
 
   constructor(private cartService: CartService){
 
@@ -18,7 +19,25 @@ export class CartViewComponent implements OnInit {
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     })
+    
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    for(let item of this.cartItems){
+      total += item.price
+    }
+    return total;
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe();
+  }
+
+  checkout(): void{
+    this.cartService.checkout(this.cartItems).subscribe();
   }
 
 }
